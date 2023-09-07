24ee6f6cfaead7f25f22226d58a01fc5d5dcc020
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2023-09-07 10:26:30 -0700

    Implemented functionality searching for people

--- canvas.py
+++ canvas.py
@@ -1,5 +1,4 @@
-from canvasapi import Canvas
-from canvasapi.course import Course, Assignment
+from canvasapi import *
 
 CANVAS_API_URL = "https://canvas.instructure.com/"
 
@@ -9,7 +8,7 @@ def test_key(api_key):
     canvas.get_current_user()
 
 
-def list_courses(api_key):
+def get_courses(api_key):
     canvas = Canvas(CANVAS_API_URL, api_key)
     user = canvas.get_current_user()
     courses = []
@@ -21,10 +20,38 @@ def list_courses(api_key):
     return courses
 
 
-def get_people(api_key):
-    courses = list_courses(api_key)
+def get_users_from_course(course):
+    people = []
+    for user in course.get_users():
+        people.append(user)
+    return people
+
+
+def get_all_users(api_key):
+    courses = get_courses(api_key)
     people = []
     for course in courses:
         for user in course.get_users():
             people.append(user)
     return people
+
+
+def search_user_in_course(course, searched_user):
+    found_users = []
+    for user in course.get_users():
+        if searched_user.lower() in user.name.lower():
+            print(user.name)
+            found_users.append(user)
+    return found_users
+
+
+def search_user_in_all(searched_user, api_key):
+    courses = get_courses(api_key)
+    found_users = []
+    for course in courses:
+        found_users.extend(search_user_in_course(course, searched_user))
+    return found_users
+
+
+def search_user_by_last_name(course, searched_user):
+    users = course.get_users().split()
--- main.py
+++ main.py
@@ -75,7 +75,7 @@ async def display_courses(message: discord.Message):
         await message.channel.send(embed=create_embed("Invalid API key!"))
         return
 
-    courses = list_courses(key)
+    courses = get_courses(key)
     courses_message = ""
     for course in courses:
         courses_message += course.name + "\n"
@@ -93,16 +93,44 @@ async def display_all_people(message: discord.Message):
         await message.channel.send(embed=create_embed("Invalid API key!"))
         return
 
-    users = get_people(key)
-    people_message = ""
-    for user in users:
-        people_message += user.name + "\n"
-    await message.channel.send(embed=create_embed("Course List", people_message))
+    courses = get_courses(key)
+    for course in courses:
+        people_message = ""
+        users = get_users_from_course(course)
+        for user in users:
+            people_message += user.name + "\n"
+        await message.channel.send(
+            embed=create_embed(f"People in {course.name}", people_message)
+        )
+
+
+async def search_people(message: discord.Message, query):
+    key = get_api_key(message.guild.id)
+    if key == "401":
+        await message.channel.send(embed=create_embed("No API key was found."))
+        return
+    try:
+        test_key(key)
+    except canvasapi.exceptions.InvalidAccessToken:
+        await message.channel.send(embed=create_embed("Invalid API key!"))
+        return
+
+    found_users = search_user_in_all(query, key)
+    if not found_users:
+        await message.channel.send(embed=create_embed("No users found!"))
+        return
+
+    found_users_message = ""
+    for user in found_users:
+        found_users_message += user.name + "\n"
+    await message.channel.send(
+        embed=create_embed(f"Users found with query `{query}`", found_users_message)
+    )
 
 
 async def display_help(message: discord.Message):
     help_description = (
-        f"`!register (api_key)` Registers your Canvas API key with the bot."
+        f"`!register [YOUR_API_KEY]` Registers your Canvas API key with the bot."
         f"This step is required for the bot to function.\n\n"
         f"`!courses` Intended for use during setup to list all possible "
         f"Canvas courses for the bot to pair with.\n\n"
@@ -116,20 +144,23 @@ async def display_help(message: discord.Message):
 async def on_message(message: discord.Message):
     if message.author == client.user or not message.guild:
         return
-    user_message = message.content
+    user_message = message.content.lower()
 
     REGISTER_LEN = len("!register")
-    if user_message.lower().startswith("!register"):
+    SEARCH_USER = len("!search-user")
+
+    if user_message.startswith("!register"):
         key = user_message[REGISTER_LEN::].strip()
         await register_user(message, key)
-    elif user_message.lower().startswith("!courses"):
+    elif user_message.startswith("!courses"):
         await display_courses(message)
-    elif user_message.lower().startswith("!all_people"):
+    elif user_message.startswith("!all-people"):
         await display_all_people(message)
-    elif user_message.lower().startswith("!help"):
+    elif user_message.startswith("!help"):
         await display_help(message)
-
-    # await message.channel.send("Hello")
+    elif user_message.startswith("!search-user"):
+        query = user_message[SEARCH_USER::].lstrip()
+        await search_people(message, query)
 
 
 @client.event
